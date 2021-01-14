a0e12fb35efe54fa6540f3c66d9d71c5ed7660e3
Grantjos12 <grantjoslyn@gmail.com>
2021-01-14 11:18:17 -0600

    added middleware for parsing form data

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require("express");
+//library for parsing through the form
+const bodyParser = require("body-parser");
 
 const app = express();
 
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Root route - GET http request
 app.get("/", (req, res) => {
-  res.send("Hi there!");
+  res.send(
+    `<div> 
+      <form method="POST">
+      <input name="email" placeholder="email"/>
+      <input name="password" placeholder="password"/>
+      <input name="passwordConfirmation" placeholder="password confirmation"/>
+      <button>Sign Up</button>
+      </form>
+      </div>`
+  );
+});
+
+//watch for / and method post
+// get access to email, pass, and confirmation
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Account created!");
 });
 
 //Start listening at localhost:3000
