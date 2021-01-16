705b62ef17fb1ccc192bd40fe7714f7578d72f6b
Grantjos12 <grantjoslyn@gmail.com>
2021-01-16 13:19:22 -0600

    schema made

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//Connect to DB
+const db = require("./config/keys").MongoURI;
+
+//Connect to Mongo with mongoose
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("DB connected!"))
+  .catch((err) => console.log(err));
 
 //ejs middlewearLayouts
 app.use(expressLayouts);
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://Grantjos12:dbgrantjos12@cluster0.omzjl.mongodb.net/E-ssentials?retryWrites=true&w=majority",
+};
